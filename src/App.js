0bb825565144237385f0fdb2b1e2b0f37bce2551
kandosa <illiteratehuang@gmail.com>
2022-01-09 13:06:22 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
     .then(response => response.json())
     .then((response) => {
       if (response) {
-          fetch('https://safe-basin-83516.herokuapp.com:3001/image', {
+          fetch('https://safe-basin-83516.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
